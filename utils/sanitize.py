0c6269914835bebe6c02d6450b00ba47acb01353
Abhishek Hiremath <abhishekaf3421@gmail.com>
2025-09-11 20:57:53 +0530

    Update PDF filename and enhance sanitization logic in utils

--- utils/sanitize.py
+++ utils/sanitize.py
@@ -4,7 +4,9 @@ def sanitize_pdf(filename: str) -> str:
     
     name=filename.rsplit(".",1)[0]
     name=name.replace(" ","_")
-    name = re.sub(r"[*a-zA-Z0-9._-]","",name)
-    if not name:
-        name = "default_collection"
+    name = re.sub(r"[^a-zA-Z0-9._-]","",name)
+    name = re.sub(r"^[._-]+","",name)
+    name=re.sub(r"[._-]+$","",name)
+    if len(name)<3:
+        name = "default_collection_"+name if name else "default_collection"
     return name
